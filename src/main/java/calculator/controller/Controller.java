9c55c9130962580635c5587aa1b8c1e510605115
윤민수 <koelysa5696@naver.com>
2025-10-19 17:32:13 +0900

    feat: 기본 입력 검증 구현
    - 커스텀 구분자를 지정하는 지 검증
    - 기본 문자열인지 검증
    - 모두 해당하지 않을 시 예외 처리

--- src/main/java/calculator/controller/Controller.java
+++ src/main/java/calculator/controller/Controller.java
@@ -1,7 +1,14 @@
 package calculator.controller;
 
+import calculator.domain.CustomWordSplit;
+import calculator.domain.DefaultWordSplit;
+import calculator.view.InputView;
+import calculator.view.OutputView;
 
 public class Controller {
+    private static final int FIRST_LETTER = 0;
+    private static final int CUSTOM_SETTING_LENGTH = 5;
+    private static final char CUSTOM_SETTING_PREFIX = '/';
 
     public void run() {
         String inputWord = InputView.Input();
@@ -9,5 +16,19 @@ public class Controller {
         OutputView.output(result);
     }
 
+    public static int firstWordValidation(String inputWord) {
+        if (inputWord == null || inputWord.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+
+        if (inputWord.charAt(FIRST_LETTER) == CUSTOM_SETTING_PREFIX && inputWord.length() > CUSTOM_SETTING_LENGTH) {
+            return CustomWordSplit.customSplit(inputWord);
+        }
+        if (Character.isDigit(inputWord.charAt(FIRST_LETTER))) {
+            return DefaultWordSplit.defaultSplit(inputWord);
+        }
+
+        throw new IllegalArgumentException();
+    }
 }
 

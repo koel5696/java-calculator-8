d144fd04011dacc051d91e521215ca7794fd602c
윤민수 <koelysa5696@naver.com>
2025-10-19 17:35:10 +0900

    feat: 올바른 숫자 형식 검증 구현
    - 숫자 검증을 위한 서비스 구현
        1. 0으로 시작하는 2자리 이상 숫자 처리
        2. 올바르지 않은 형식 처리
        3. 음수 처리

---
+++ src/main/java/calculator/domain/numberValidation.java
@@ -0,0 +1,41 @@
+package calculator.domain;
+
+import static calculator.constants.ErrorMessage.INPUT_FORMAT_ERROR;
+import static calculator.constants.ErrorMessage.NEGATIVE_NUMBER_ERROR;
+import static calculator.constants.ErrorMessage.ZERO_START_NUMBER_ERROR;
+
+public class numberValidation {
+    private static final char ZERO = '0';
+    private static final int START_INDEX = 0;
+    private static final int NUMBER_LENGTH = 2;
+
+    public static int[] parseIntService(String[] numbers) {
+        int[] finalNumber = new int[numbers.length];
+        for (int i = START_INDEX; i < numbers.length; i++) {
+            zeroStartValidation(numbers[i]);
+            finalNumber[i] = numberFormatValidation(numbers[i]);
+            negativeValidation(finalNumber[i]);
+        }
+        return finalNumber;
+    }
+
+    public static void zeroStartValidation(String number) {
+        if (number.length() > NUMBER_LENGTH && number.charAt(START_INDEX) == ZERO) {
+            throw new IllegalArgumentException(ZERO_START_NUMBER_ERROR.getMessage());
+        }
+    }
+
+    public static int numberFormatValidation(String number) {
+        try {
+            return Integer.parseInt(number);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(number + INPUT_FORMAT_ERROR.getMessage());
+        }
+    }
+
+    public static void negativeValidation(int number) {
+        if (number < 0) {
+            throw new IllegalArgumentException(NEGATIVE_NUMBER_ERROR.getMessage());
+        }
+    }
+}

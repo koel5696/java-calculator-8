51b73196e419d5b6aef6a3adcef714bdb8ddcbad
윤민수 <koelysa5696@naver.com>
2025-10-20 20:49:10 +0900

    feat: 패턴 매칭 상수 정의
    - pattern타입 상수 생성
    - 커스텀 구분자 유무 확인 표현식 상수화

--- src/main/java/calculator/constants/RegexPattern.java
+++ src/main/java/calculator/constants/RegexPattern.java
@@ -1,17 +1,27 @@
 package calculator.constants;
 
+import java.util.regex.Pattern;
+
 public enum RegexPattern {
+    CUSTOM_SETTING_PREFIX("//"),
     CUSTOM_REGEX_PATTEN("//\\D\\\\n"),
+    NUMBER_VALIDATE("\\d+"),
     REGEX_OR("|"),
     DEFAULT_REGEX_PATTEN(",|:");
 
-    private final String regexPattern;
+    private final String regex;
+    private final Pattern pattern;
 
     RegexPattern(String regexPattern) {
-        this.regexPattern = regexPattern;
+        this.regex = regexPattern;
+        this.pattern = Pattern.compile(regexPattern);
     }
 
-    public String getRegexPattern() {
-        return regexPattern;
+    public String getRegex() {
+        return regex;
+    }
+
+    public Pattern getRegexPattern() {
+        return pattern;
     }
 }

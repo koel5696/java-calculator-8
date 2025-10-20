609c4588002ad55ea1ce7db77a43f8e27287822b
윤민수 <koelysa5696@naver.com>
2025-10-20 22:38:05 +0900

    refactor: 사용하지 않는 상수 삭제

--- src/main/java/calculator/constants/ErrorMessage.java
+++ src/main/java/calculator/constants/ErrorMessage.java
@@ -5,8 +5,7 @@ public enum ErrorMessage {
     CUSTOM_SETTING_ERROR("커스텀 구분자는 숫자가 아닌 문자(공백 포함) 1개를 문자열 처음 \"//\"와 \"\\n\" 사이에 지정해야 합니다."),
     ZERO_START_NUMBER_ERROR("와 같은 숫자 형식은 올바르지 않습니다."),
     OVERFLOW_ERROR("숫자의 허용 범위를 초과했습니다."),
-    NEGATIVE_NUMBER_ERROR("음수는 허용되지 않습니다."),
-    ONLY_NATURAL_NUMBER_ERROR("0 혹은 양의 정수만 가능합니다.");
+    ONLY_NATURAL_NUMBER_ERROR("잘못된 문자(숫자) 입력: 0 혹은 양의 정수만 가능합니다.");
 
     private final String message;
 

e0edab8761b65644db9bfddc25498e7f39f72090
윤민수 <koelysa5696@naver.com>
2025-10-20 15:36:58 +0900

    feat: 오버플로우 측정 메서드 추가

--- src/main/java/calculator/domain/NumberSum.java
+++ src/main/java/calculator/domain/NumberSum.java
@@ -1,9 +1,22 @@
 package calculator.domain;
 
-import java.util.Arrays;
+import static calculator.constants.ErrorMessage.SUM_OVERFLOW;
 
 public class NumberSum {
+    private static final int INITIAL_TOTAL = 0;
+
     public static int sum(int[] finalNumbers) {
-        return Arrays.stream(finalNumbers).sum();
+        int total = INITIAL_TOTAL;
+        for (int number : finalNumbers) {
+            CheckOverflow(total, number);
+            total += number;
+        }
+        return total;
+    }
+
+    private static void CheckOverflow(int total, int number) {
+        if (number > 0 && total > Integer.MAX_VALUE - number) {
+            throw new IllegalArgumentException(SUM_OVERFLOW.getMessage());
+        }
     }
 }

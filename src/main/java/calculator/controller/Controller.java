cbc348dfc6bfadb360229efa636a94caaf2b26ec
윤민수 <koelysa5696@naver.com>
2025-10-20 15:40:46 +0900

    fix: 첫 숫자 음수 판별 수정

--- src/main/java/calculator/controller/Controller.java
+++ src/main/java/calculator/controller/Controller.java
@@ -1,22 +1,27 @@
 package calculator.controller;
 
+import static calculator.constants.ErrorMessage.INPUT_STRING_ERROR;
+
 import calculator.domain.CustomWordSplit;
 import calculator.domain.DefaultWordSplit;
+import calculator.domain.NumberValidation;
 import calculator.view.InputView;
 import calculator.view.OutputView;
 
 public class Controller {
     private static final int FIRST_LETTER = 0;
     private static final int CUSTOM_SETTING_LENGTH = 5;
+    private static final int NEGATIVE_NUMBER = -1;
     private static final char CUSTOM_SETTING_PREFIX = '/';
+    private static final char MINUS = '-';
 
     public void run() {
         String inputWord = InputView.Input();
-        int result = firstWordValidation(inputWord);
+        int result = selectCustomOrDefault(inputWord);
         OutputView.output(result);
     }
 
-    public static int firstWordValidation(String inputWord) {
+    private static int selectCustomOrDefault(String inputWord) {
         if (inputWord == null || inputWord.isEmpty()) {
             throw new IllegalArgumentException();
         }
@@ -24,11 +29,16 @@ public class Controller {
         if (inputWord.charAt(FIRST_LETTER) == CUSTOM_SETTING_PREFIX && inputWord.length() > CUSTOM_SETTING_LENGTH) {
             return CustomWordSplit.customSplitService(inputWord);
         }
+
         if (Character.isDigit(inputWord.charAt(FIRST_LETTER))) {
             return DefaultWordSplit.defaultSplit(inputWord);
         }
 
-        throw new IllegalArgumentException();
+        if (inputWord.charAt(FIRST_LETTER) == MINUS) {
+            NumberValidation.negativeValidation(NEGATIVE_NUMBER);
+        }
+
+        throw new IllegalArgumentException(INPUT_STRING_ERROR.getMessage());
     }
 }
 

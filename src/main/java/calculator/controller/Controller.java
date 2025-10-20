dc0aeb4cf8d3d6331e2ed8827be21b46d0d2c488
윤민수 <koelysa5696@naver.com>
2025-10-20 20:37:01 +0900

    refactor: 컨트롤러 전면 수정
    - view와 service를 오가며 전달 역할
    - 검증 기능 모두 서비스로 변경

--- src/main/java/calculator/controller/Controller.java
+++ src/main/java/calculator/controller/Controller.java
@@ -1,44 +1,18 @@
 package calculator.controller;
 
-import static calculator.constants.ErrorMessage.INPUT_STRING_ERROR;
-
-import calculator.domain.CustomWordSplit;
-import calculator.domain.DefaultWordSplit;
-import calculator.domain.NumberValidation;
+import calculator.domain.CalculatorService;
+import calculator.domain.SplitService;
 import calculator.view.InputView;
 import calculator.view.OutputView;
 
 public class Controller {
-    private static final int FIRST_LETTER = 0;
-    private static final int CUSTOM_SETTING_LENGTH = 5;
-    private static final int NEGATIVE_NUMBER = -1;
-    private static final char CUSTOM_SETTING_PREFIX = '/';
-    private static final char MINUS = '-';
 
     public void run() {
         String inputWord = InputView.Input();
-        int result = selectCustomOrDefault(inputWord);
+        CalculatorService calculatorService = new CalculatorService(inputWord);
+        SplitService splitService = calculatorService.selectCustomOrDefault();
+        int result = calculatorService.numberValidService(splitService.split(inputWord));
         OutputView.output(result);
-    }
-
-    private static int selectCustomOrDefault(String inputWord) {
-        if (inputWord == null || inputWord.isEmpty()) {
-            throw new IllegalArgumentException();
-        }
-
-        if (inputWord.charAt(FIRST_LETTER) == CUSTOM_SETTING_PREFIX && inputWord.length() > CUSTOM_SETTING_LENGTH) {
-            return CustomWordSplit.customSplitService(inputWord);
-        }
-
-        if (Character.isDigit(inputWord.charAt(FIRST_LETTER))) {
-            return DefaultWordSplit.defaultSplit(inputWord);
-        }
-
-        if (inputWord.charAt(FIRST_LETTER) == MINUS) {
-            NumberValidation.negativeValidation(NEGATIVE_NUMBER);
-        }
-
-        throw new IllegalArgumentException(INPUT_STRING_ERROR.getMessage());
     }
 }
 

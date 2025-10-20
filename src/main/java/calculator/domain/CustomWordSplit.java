db57ce3a536600f5e3e64984370d9664a881df70
윤민수 <koelysa5696@naver.com>
2025-10-20 20:47:27 +0900

    refactor: 각 방식에 따른 숫자 분리 오버라이드 수정
    - 커스텀 split 수정
      1. split 메서드 오버라이드를 통한 재정의
      2. 구분자 지정 표현 패턴 매칭 최적화
      3. 모든 메서드 인스턴스화
    - 기본 split 오버라이드 수정

--- src/main/java/calculator/domain/CustomWordSplit.java
+++ src/main/java/calculator/domain/CustomWordSplit.java
@@ -4,36 +4,33 @@ import static calculator.constants.ErrorMessage.CUSTOM_SETTING_ERROR;
 import static calculator.constants.RegexPattern.CUSTOM_REGEX_PATTEN;
 import static calculator.constants.RegexPattern.DEFAULT_REGEX_PATTEN;
 import static calculator.constants.RegexPattern.REGEX_OR;
-import static calculator.domain.NumberSum.sum;
-import static calculator.domain.NumberValidation.parseIntService;
 
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class CustomWordSplit {
-    private static final int CUSTOM_SETTING_START = 0;
-    private static final int CUSTOM_SETTING_LENGTH = 5;
+public class CustomWordSplit implements SplitService {
     private static final int STRING_TO_CALCULATE = 5;
     private static final int CUSTOM_DELIMITER_INDEX = 2;
 
-    public static int customSplitService(String inputWord) {
+    @Override
+    public String[] split(String inputWord) {
         char customDelimiter = customFormatValidation(inputWord);
         String word = inputWord.substring(STRING_TO_CALCULATE);
-        String[] numbers = combineDelimiter(word, customDelimiter);
-        return sum(parseIntService(numbers));
+        return combineDelimiter(word, customDelimiter);
     }
 
-    public static char customFormatValidation(String inputWord) {
-        String customization = inputWord.substring(CUSTOM_SETTING_START, CUSTOM_SETTING_LENGTH);
-        if (!customization.matches(CUSTOM_REGEX_PATTEN.getRegexPattern())) {
+    private char customFormatValidation(String inputWord) {
+        Matcher matcher = CUSTOM_REGEX_PATTEN.getRegexPattern().matcher(inputWord);
+        if (!matcher.lookingAt()) {
             throw new IllegalArgumentException(CUSTOM_SETTING_ERROR.getMessage());
         }
-        return customization.charAt(CUSTOM_DELIMITER_INDEX);
+        return inputWord.charAt(CUSTOM_DELIMITER_INDEX);
     }
 
-    public static String[] combineDelimiter(String word, char customDelimiter) {
+    private String[] combineDelimiter(String word, char customDelimiter) {
         String quotedCustomDelimiter = Pattern.quote(String.valueOf(customDelimiter));
         String combineDelimiterRegex =
-                DEFAULT_REGEX_PATTEN.getRegexPattern() + REGEX_OR.getRegexPattern() + quotedCustomDelimiter;
+                DEFAULT_REGEX_PATTEN.getRegex() + REGEX_OR.getRegex() + quotedCustomDelimiter;
         return word.split(combineDelimiterRegex);
     }
 }
--- src/main/java/calculator/domain/DefaultWordSplit.java
+++ src/main/java/calculator/domain/DefaultWordSplit.java
@@ -1,15 +1,12 @@
 package calculator.domain;
 
 import static calculator.constants.RegexPattern.DEFAULT_REGEX_PATTEN;
-import static calculator.domain.NumberSum.sum;
-import static calculator.domain.NumberValidation.parseIntService;
 
-public class DefaultWordSplit {
 
-    public static int defaultSplit(String inputWord) {
-        String[] words = inputWord.split(DEFAULT_REGEX_PATTEN.getRegexPattern());
-        return sum(parseIntService(words));
+public class DefaultWordSplit implements SplitService {
+
+    @Override
+    public String[] split(String inputWord) {
+        return inputWord.split(DEFAULT_REGEX_PATTEN.getRegex());
     }
-
-
 }

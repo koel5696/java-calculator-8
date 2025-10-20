288de8f85cb20c98b213b8dfe617c59f7588d80c
윤민수 <koelysa5696@naver.com>
2025-10-20 22:44:54 +0900

    test: 추가 케이스 테스트
    - 공백(space) 테스트
    - 구분자 오입력 테스트
    - 오버플로우 테스트
    - 숫자를 구분자로 사용 테스트

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -1,11 +1,11 @@
 package calculator;
 
-import camp.nextstep.edu.missionutils.test.NsTest;
-import org.junit.jupiter.api.Test;
-
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Test;
 
 class ApplicationTest extends NsTest {
     @Test
@@ -17,10 +17,42 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
+    void 커스텀_구분자_사용_띄어쓰기() {
+        assertSimpleTest(() -> {
+            run("// \\n1 2:3");
+            assertThat(output()).contains("결과 : 6");
+        });
+    }
+
+    @Test
+    void 커스텀_구분자_사용_예외() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("//^\\n1^2&3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 오버플로우_예외() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("//^\\n2147483647^10"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 숫자_구분자_예외() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("//8\\n18283"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
     void 예외_테스트() {
         assertSimpleTest(() ->
-            assertThatThrownBy(() -> runException("-1,2,3"))
-                .isInstanceOf(IllegalArgumentException.class)
+                assertThatThrownBy(() -> runException("-1,2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
         );
     }
 
